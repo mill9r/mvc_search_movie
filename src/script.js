f2552adb9b09a72915c7819caf61158d507e2311
Andrei Tsylko <Andrei.Tsylko@aerlingus.com>
2020-05-27 19:15:53 +0300

    add composite implementation

--- src/script.js
+++ src/script.js
@@ -10,27 +10,29 @@ class Model {
     getMovies(search) {
         this.api.sendGetRequest(`${this.path}?search=${search}&searchBy=title`)
             .then(data => {
-                this.onSearchMovieChanged(Utils.extractDataFields(data.data,'title'))
+                this.onSearchMovieChanged(Utils.extractDataFields(data.data, 'title'))
             })
     }
 
-    bindMoviesSearchUpdate(callback){
+    bindMoviesSearchUpdate(callback) {
         this.onSearchMovieChanged = callback
     }
 }
 
 class View {
-    constructor() {
+    constructor(form) {
         this.app = this.getElement('#root');
-        this.form = this.createElement('form');
-        this.input = this.createElement('input');
+        this.form = form
+        // this.form = this.createElement('form');
+        // this.input = this.createElement('input');
         this.movies = this.createElement('ul')
-        this.submitButton = this.createElement('button', '', 'Search')
-        this.input.type = 'text';
-        this.input.placeholder = 'Enter search criteria';
-        this.input.name = 'search';
-        this.form.append(this.input, this.submitButton);
-        this.app.append(this.form, this.movies);
+        // this.submitButton = this.createElement('button', '', 'Search')
+        // this.input.type = 'text';
+        // this.input.placeholder = 'Enter search criteria';
+        // this.input.name = 'search';
+        // this.form.append(this.input, this.submitButton);
+        // this.form.add( this.submitButton);
+        this.app.append(this.form.getElement(), this.movies);
         this.render();
     }
 
@@ -55,7 +57,7 @@ class View {
     }
 
     getSearchText() {
-        return this.input.value;
+        return this.form.getChild(0).getValue();
     }
 
     resetInput() {
@@ -63,7 +65,7 @@ class View {
     }
 
     bindSearch(handler) {
-        this.form.addEventListener('submit', event => {
+        this.form.getElement().addEventListener('submit', event => {
             event.preventDefault();
 
             if (this.getSearchText()) {
@@ -121,14 +123,124 @@ class Api {
 }
 
 class Utils {
-    static extractDataFields(list,field){
-        return list.reduce((acc,item)=>{
+    static extractDataFields(list, field) {
+        return list.reduce((acc, item) => {
             acc.push(item[field])
             return acc;
-        },[])
+        }, [])
     }
 
 }
 
+class Composite {
+    add(child) {
+    };
 
-const app = new Controller(new Model(new Api(BASE_URL), PATH), new View())
+    remove(child) {
+    };
+
+    getChild(child) {
+    };
+}
+
+class FormItem extends Composite {
+    save() {
+    }
+}
+
+class CompositeForm extends FormItem {
+    constructor(id, method, action) {
+        super();
+        this.formComponents = [];
+        this.element = document.createElement('form');
+        this.element.id = id;
+        this.element.method = method || 'POST';
+        this.element.action = action || '#';
+    }
+
+    add(child) {
+        this.formComponents.push(child);
+        this.element.append(child.getElement());
+    }
+
+    remove(child) {
+        this.formComponents = this.formComponents.filter(component => component !== child);
+    }
+
+    getChild(i) {
+        return this.formComponents[i];
+    }
+
+    save() {
+        this.formComponents.forEach(component => component.save());
+    }
+
+    getElement() {
+        return this.element;
+    }
+}
+
+class Field extends FormItem {
+    constructor(id) {
+        super()
+        this.id = id;
+        this.element;
+    }
+
+    getElement() {
+        return this.element;
+    }
+
+    getValue() {
+        throw new Error('Unsupported operation on the class Field')
+    }
+}
+
+class InputField extends Field {
+    constructor(id,label,placeholder,cssStyle) {
+        super(id);
+        this.input = document.createElement('input');
+        this.input.id = id;
+        this.input.placeholder = placeholder;
+
+        this.label = document.createElement('label');
+        const labelTextNode = document.createTextNode(label);
+        this.label.append(labelTextNode);
+
+        this.element = document.createElement('div');
+        this.element.className = cssStyle;
+        this.element.append(this.label);
+        this.element.append(this.input);
+    }
+
+    getValue() {
+        return this.input.value;
+    }
+}
+// Stub
+class TextArea extends Field{
+    constructor() {
+        super();
+    }
+}
+
+class Button extends Composite{
+     constructor(name,className='') {
+         super();
+         this.element = document.createElement('button');
+         this.element.innerText = name;
+         this.element.className = className;
+     }
+
+    getElement() {
+        return this.element;
+    }
+}
+
+
+
+
+const form = new CompositeForm('search-form','GET')
+form.add(new InputField('search-field','','Enter search'))
+form.add(new Button('Search','',))
+const app = new Controller(new Model(new Api(BASE_URL), PATH), new View(form))
